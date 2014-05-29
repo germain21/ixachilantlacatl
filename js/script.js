96b4e5f249f4c89a10b620183aa8d620f4505dee
germain21 <pgmon21@gmail.com>
2014-05-28 21:33:32 -0500

    avance2
    menu principal terminado

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 $(document).ready(function(){
-		$('#coin-slider').coinslider({ width: 900, navigation: false, delay: 5000 });
-	});
+        $('.carousel').carousel({
+            interval: 3000
+        });
+    });
